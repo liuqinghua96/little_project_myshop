4475f891190b673d4fc37a56d2552c86bada3e93
liuqinghua96 <865708365@qq.com>
2019-06-29 21:35:59 +0800

    搜索跳转功能

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -3,22 +3,35 @@
     <div class="search">
       <div class="search-content">
         <div class="search-input">
-          <icon type='search' size='16'/>
-          <input @confirm='handleConfirm' @input='handleInput' v-model='keyword' placeholder="请输入关键字" />
+          <icon type='search'
+                size='16' />
+          <input @confirm='handleConfirm'
+                 @input='handleInput'
+                 v-model='keyword'
+                 placeholder="请输入关键字" />
         </div>
-        <button class="cancel" v-if='keyword' @click="deleteKeyword">取消</button>
-        <div class="search-modal" v-show="keyword">
-          <div :key='item.goods_id' v-for='item in resultList' class="search-item">{{item.goods_name}}</div>
+        <button class="cancel"
+                v-if='keyword'
+                @click="deleteKeyword">取消</button>
+        <div class="search-modal"
+             v-show="keyword">
+          <div :key='item.goods_id'
+               v-for='item in resultList'
+               class="search-item">{{item.goods_name}}</div>
         </div>
       </div>
     </div>
     <div class="history">
       <h4>搜索历史</h4>
-      <icon @click='clearHandle' type='clear' size='16'/>
+      <icon @click='clearHandle'
+            type='clear'
+            size='16' />
     </div>
     <div class="history-list">
-      <div :key='index' v-for='(item, index) in keywordHistory' class="history-item">
-      {{item}}
+      <div :key='index'
+           v-for='(item, index) in keywordHistory'
+           class="history-item">
+        <navigator :url="'/pages/searchList/main?keyword=' + item">{{item}}</navigator>
       </div>
     </div>
   </div>
@@ -43,7 +56,7 @@ export default {
       // 函数防抖，限制请求次数
       clearTimeout(this.timer)
       this.timer = setTimeout(async () => {
-        let resultRes = await request('goods/qsearch', 'GET', {query: this.keyword})
+        let resultRes = await request('goods/qsearch', 'GET', { query: this.keyword })
         this.resultList = resultRes.data.message
       }, 1000)
     },
@@ -51,6 +64,9 @@ export default {
       this.keywordHistory.unshift(this.keyword)
       this.keywordHistory = [...new Set(this.keywordHistory)]
       mpvue.setStorageSync('history', this.keywordHistory)
+      mpvue.navigateTo({
+        url: '/pages/searchList/main?keyword=' + this.keyword
+      })
     },
     clearHandle () {
       mpvue.clearStorage()
@@ -60,5 +76,5 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-  @import './main.less';
+@import "./main.less";
 </style>
---
+++ src/pages/searchList/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{keyword}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      keyword: ''
+    }
+  },
+  onLoad (params) {
+    this.keyword = params.keyword
+  }
+}
+</script>
